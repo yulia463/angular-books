5896566b6b8994013a4ba929e2d00c8fd7bf52e3
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-09-19 18:39:30 +0200

    add book-detail

---
+++ src/app/pages/book-detail/book-detail.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input} from '@angular/core';
+import { IProduct } from '../../models/product';
+import { ActivatedRoute } from '@angular/router';
+import {ProductsService} from "../../services/products.service";
+
+
+@Component({
+  selector: 'app-book-detail',
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.scss']
+})
+export class BookDetailComponent {
+  @Input() product?: IProduct;
+
+  constructor(
+    private route: ActivatedRoute, private productsService: ProductsService
+  ) {
+    const productId = Number(this.route.snapshot.params['id'])
+    this.product = productsService?.products.find(product => product.id === productId)
+  }
+}
