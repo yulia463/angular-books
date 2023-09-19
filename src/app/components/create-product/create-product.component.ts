3fb956d8808df53cd1296ab71893bf001f5f5f56
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-09-19 21:47:55 +0200

    add select for author

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,16 +1,56 @@
-import {Component} from '@angular/core';
-import {ProductsService} from "../../services/products.service";
+// import {Component} from '@angular/core';
+// import {ProductsService} from "../../services/products.service";
+// import {ModalService} from "../../services/modal.service";
+// import {products} from "src/app/data/products";
+// import {IProduct} from "src/app/models/product";
+//
+// @Component({
+//   selector: 'app-create-product',
+//   templateUrl: './create-product.component.html',
+//   styleUrls: ['./create-product.component.scss']
+// })
+//
+// export class CreateProductComponent {
+//   newBook: IProduct = {
+//     id: 0,
+//     title: '',
+//     author: '',
+//     description: '',
+//     genre: '',
+//     image: 'https://i.pravatar.cc',
+//     language: '',
+//     pages: 0,
+//   };
+//   constructor(private productService: ProductsService, private modalService: ModalService) {}
+//
+//   onSubmit() {
+//     products.unshift(this.newBook);
+//     this.newBook = {
+//       id: 0,
+//       title: '',
+//       author: '',
+//       description: '',
+//       genre: '',
+//       image: 'https://i.pravatar.cc',
+//       language: '',
+//       pages: 0,
+//     };
+//     this.modalService.close()
+//   }
+// }
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { IProduct } from '../../models/product';
 import {ModalService} from "../../services/modal.service";
 import {products} from "src/app/data/products";
-import {IProduct} from "src/app/models/product";
-
 @Component({
   selector: 'app-create-product',
   templateUrl: './create-product.component.html',
-  styleUrls: ['./create-product.component.scss']
+  styleUrls: ['./create-product.component.scss'],
 })
 
-export class CreateProductComponent {
+export class CreateProductComponent implements OnInit {
+  selectedAuthor: string = '';
   newBook: IProduct = {
     id: 0,
     title: '',
@@ -21,9 +61,21 @@ export class CreateProductComponent {
     language: '',
     pages: 0,
   };
-  constructor(private productService: ProductsService, private modalService: ModalService) {}
+
+  authors: string[] = [];
+
+  constructor(
+    private productService: ProductsService,
+    private modalService: ModalService
+  ) {}
+
+  ngOnInit(): void {
+    this.authors = this.productService.getAuthors();
+  }
 
   onSubmit() {
+    this.newBook.author = this.selectedAuthor;
+
     products.unshift(this.newBook);
     this.newBook = {
       id: 0,
@@ -35,6 +87,6 @@ export class CreateProductComponent {
       language: '',
       pages: 0,
     };
-    this.modalService.close()
+    this.modalService.close();
   }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,3 +1,36 @@
+// import { Injectable } from '@angular/core';
+// import { Observable, of, throwError } from 'rxjs';
+// import { catchError, delay, retry, tap } from 'rxjs/operators';
+// import { IProduct } from '../models/product';
+// import { ErrorService } from './error.service';
+// import { products } from '../data/products';
+//
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class ProductsService {
+//   constructor(private errorService: ErrorService) {}
+//
+//   products: IProduct[] = products;
+//
+//   getAll(): Observable<Array<IProduct>> {
+//     return of(this.products).pipe(
+//       delay(2000),
+//       retry(2),
+//       catchError(this.errorHandler.bind(this))
+//     );
+//   }
+//
+//   create(product: IProduct): Observable<IProduct> {
+//     this.products.push(product);
+//     return of(product);
+//   }
+//
+//   private errorHandler(error: any) {
+//     this.errorService.handle(error.message);
+//     return throwError(() => error.message);
+//   }
+// }
 import { Injectable } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
 import { catchError, delay, retry, tap } from 'rxjs/operators';
@@ -24,6 +57,16 @@ export class ProductsService {
   create(product: IProduct): Observable<IProduct> {
     this.products.push(product);
     return of(product);
+  }
+
+  getAuthors(): string[] {
+    const uniqueAuthors = new Set<string>();
+
+    this.products.forEach((product) => {
+      uniqueAuthors.add(product.author);
+    });
+
+    return Array.from(uniqueAuthors); // Преобразуйте Set в массив строк
   }
 
   private errorHandler(error: any) {
